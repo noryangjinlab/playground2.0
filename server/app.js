f0a3278278027ec2b961971879d78d01295b4db2
h-lawliet <whoish5252@naver.com>
2026-01-29 17:27:56 +0900

    qwfwef

--- server/app.js
+++ server/app.js
@@ -51,9 +51,7 @@ app.use(express.static(clientBuildPath));
 app.use('/auth', authRouter);
 app.use('/lab', labRouter);
 
-app.get('(.*)', (req, res) => {
-  if (req.path.startsWith('/assets')) return res.sendStatus(404);
-  if (path.extname(req.path)) return res.sendStatus(404);
+app.get('/:path*', (req, res) => {
   res.sendFile(path.join(clientBuildPath, 'index.html'));
 });
 
